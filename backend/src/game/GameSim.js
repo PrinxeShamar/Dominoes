30b9f8f798a007f0a955ae15f4e14fdf9faaa5e3
19690ao <ortaa@rpi.edu>
2022-10-27 16:54:09 -0400

    Add Rule Classes

--- backend/src/game/GameSim.js
+++ backend/src/game/GameSim.js
@@ -1,7 +1,30 @@
 import Board from "./gamesim/Board";
+import DominoSet from "./gamesim/DominoSet";
+import MatchSim from "./gamesim/MatchSim";
+import Player from "./gamesim/Player";
+import DrawRuleSet from "./gamesim/ruleset/DrawRuleSet";
+import BlockRuleSet from "./gamesim/ruleset/BlockRuleSet";
 
+/**
+ * GameSim takes raw game parameters that define the
+ * way the game is played and uses them to create
+ * and control the game itself.
+ */
 class GameSim {
-  constructor() {
+  constructor(ruleName, playerCount, customRules) {
     this.board = new Board();
+    this.dominoSet = new DominoSet(dRange);
+    this.matchSim = new MatchSim();
+    this.players = Player[playerCount];
+    this.ruleSet = mapRule(ruleName, customRules);
+  }
+
+  mapRule(ruleStr, customRules) {
+    switch (toLowerCase(ruleStr)) {
+      case "draw":
+        return new DrawRuleSet(customRules);
+      case "block":
+        return new BlockRuleSet(customRules);
+    }
   }
 }
---
+++ backend/src/game/gamesim/ruleset/DrawRuleSet.js
@@ -0,0 +1,3 @@
+class DrawRuleSet {
+  constructor() {}
+}
