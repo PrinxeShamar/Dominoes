56a3010eac394d39be612f5df71ce0ae51fa5e3e
19690ao <ortaa@rpi.edu>
2022-10-27 18:45:14 -0400

    Add RuleSet Interface

--- backend/src/game/gamesim/RuleSet.js
+++ backend/src/game/gamesim/RuleSet.js
@@ -1,3 +1,20 @@
 export default class RuleSet {
-  constructor(customRules) {}
+  constructor(customRules) {
+    this.playerCount = customRules.get("playerCount");
+    this.dRangeStart = customRules.get("dRangeStart");
+    this.dRangeEnd = customRules.get("dRangeEnd");
+    this.stopCondition = customRules.get("stopCondition");
+    this.handSize = customRules.get("handSize");
+    varCleanUp();
+  }
+
+  varCleanUp() {}
+
+  get dRangeStart() {
+    return dRangeStart;
+  }
+
+  get dRangeEnd() {
+    return dRangeEnd;
+  }
 }
