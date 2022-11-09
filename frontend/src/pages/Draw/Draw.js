d8d361fe92907e39ab659456a89c88b148ea2386
Corey Quinby <coreyapplequinby@gmail.com>
2022-11-09 15:35:28 -0500

    homepage logo on block and draw pages

--- frontend/src/pages/Draw/Draw.js
+++ frontend/src/pages/Draw/Draw.js
@@ -1,6 +1,12 @@
+import logo from "../../images/logo/logo.png"
+import { Link } from "react-router-dom";
+
 export default function Draw() {
   return (
     <div>
+      <Link to="/" id="logo">
+        <img src={logo} />
+      </Link>
       <h1>Draw</h1>
     </div>
   );
